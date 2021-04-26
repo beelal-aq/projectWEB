43ce8a9695d7c83164d698c77561454eb7e637fa
Bilal Abdul Qowy <57736120+beelal-aq@users.noreply.github.com>
2021-04-26 18:36:51 +0700

    Add files via upload

---
+++ admin/index-backend.php
@@ -0,0 +1,36 @@
+<?php include('partials/navbar.php');?>
+        
+        <div class = "main-content">
+            <h1>DASHBOARD</h1>
+
+            <div class = "column text-center">
+                <h1>5</h1>
+                <br/>
+                Alumni
+            </div>
+
+            <div class = "column text-center">
+                <h1>5</h1>
+                <br/>
+                Alumni
+            </div>
+
+            <div class = "column text-center">
+                <h1>5</h1>
+                <br/>
+                Alumni
+            </div>
+
+            <div class = "column text-center">
+                <h1>5</h1>
+                <br/>
+                Alumni
+            </div>
+
+            <div class = "clearfix"></div>
+
+        </div>
+
+        <?php include('partials/footer.php');?>
+
+      
---
+++ admin/partials/navbar.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <title> Alumni ESQBS</title>
+
+        <link rel="stylesheet" href="../design/admin.css">
+    </head>
+
+    <body>
+        <div class = "menu text-center">
+            <div class = "wrapper">
+                <ul>
+                    <li> <a href="index-backend.php">Home</a></li>
+                    <li> <a href="admin.php">Admin</a></li>
+                    <li> <a href="career.php">Career</a></li>
+                    <li> <a href="alumni.php">Alumni</a></li>
+                    <li> <a href="regis.php">Registration</a></li>
+                </ul>
+
+            </div>
+        </div>
